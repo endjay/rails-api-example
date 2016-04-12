5f5d202e9965febe2da5424dc97c647107eb8bb2
gusti amanda <gusti@go-jek.com>
2016-04-12 16:32:51 +0700

    create model artikel

---
+++ app/controllers/artikels_controller.rb
@@ -0,0 +1,59 @@
+class ArtikelsController < ApplicationController
+  before_action :set_artikel, only: [:show, :update, :destroy]
+
+  # GET /artikels
+  # GET /artikels.json
+  def index
+    @artikels = Artikel.all
+
+    render json: @artikels
+  end
+
+  # GET /artikels/1
+  # GET /artikels/1.json
+  def show
+    render json: @artikel
+  end
+
+  # POST /artikels
+  # POST /artikels.json
+  def create
+    @artikel = Artikel.new(artikel_params)
+
+    if @artikel.save
+      render json: @artikel, status: :created, location: @artikel
+    else
+      render json: @artikel.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /artikels/1
+  # PATCH/PUT /artikels/1.json
+  def update
+    @artikel = Artikel.find(params[:id])
+
+    if @artikel.update(artikel_params)
+      head :no_content
+    else
+      render json: @artikel.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /artikels/1
+  # DELETE /artikels/1.json
+  def destroy
+    @artikel.destroy
+
+    head :no_content
+  end
+
+  private
+
+    def set_artikel
+      @artikel = Artikel.find(params[:id])
+    end
+
+    def artikel_params
+      params.require(:artikel).permit(:title, :content)
+    end
+end
---
+++ app/models/artikel.rb
@@ -0,0 +1,2 @@
+class Artikel < ActiveRecord::Base
+end

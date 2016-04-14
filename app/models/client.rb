77cb46f3e67ae1290cf4be7493ead320e88be1a7
Adji Pramono <adji@go-jek.com>
2016-04-14 15:58:32 +0700

    create client feature and test

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,11 @@
+require 'securerandom'
+
 class Client < ActiveRecord::Base
+  validates :app_name, :presence => true, :uniqueness => true, :length => { :in => 5..20 }
+  before_save :generate_app_params
+
+  def generate_app_params
+    self.app_id = SecureRandom.hex(15)
+    self.app_secret = SecureRandom.hex(20)
+  end
 end
